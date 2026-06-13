f23c4d343d3310d2897332ad73b80fed4656c865
FORGE096 <forge1803@gmail.com>
2026-06-13 10:20:04 +0330

    feat: glass card widget has been created. it will show logs with glassmorphism like ui. its simple for now

---
+++ lib/presentation/widgets/glass_card.dart
@@ -0,0 +1,30 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class GlassCard extends StatelessWidget {
+  final Widget child;
+
+  const GlassCard({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16.0),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.1),
+            borderRadius: BorderRadius.circular(16.0),
+            border: Border.all(
+              color: Colors.white.withOpacity(0.2),
+              width: 1.5,
+            ),
+          ),
+          padding: const EdgeInsets.all(16.0),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
